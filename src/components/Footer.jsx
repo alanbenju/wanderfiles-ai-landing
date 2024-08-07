4fb2480a2e971bdfa5bdc443c73d86b37873e4e2
Alan Bendjuya <alanbendjuya@Alans-MacBook-Pro.local>
2024-08-07 16:15:50 -0300

    mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,7 +44,7 @@ export const Footer = () => {
               Simplify Your File Data Extraction
               </p>
               <p className="mb-10 mt-4 sm:w-[22rem] lg:w-[20rem] xl:w-[24rem] text-gray-400 leading-loose text-center lg:text-left mx-auto lg:mx-0">
-              wanderfiles-ai@gmail.com
+              wanderfilesai@gmail.com
               </p>
               <div className="w-36 mx-auto lg:mx-0">
                 <a
